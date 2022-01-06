8200d710bf0ba72c88f585168699305464d5e65e
bananathepirate <bananathepirate@gmail.com>
2022-01-06 18:38:01 +0100

    bugfixes Popup
    IT WORKS!!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//not sure what to do with this, exerything is done with the popup
+/*not sure what to do with this, exerything is done with the popup
 
 //first extract MAL animeID from url. I am making an array with all the pathnames
 //eg https://myanimelist.net/anime/16498/Shingeki_no_Kyojin => anime/16498/Shingeki_no_Kyojin
@@ -7,3 +7,6 @@ var animeID = new URL(document.location).pathname.split("/")[1];
 var database = browser.storage.local.get("JsonUrl");
 //find the link to the anime in the database
 var link = database[animeID]
+
+*/
+
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,31 +1,16 @@
-//shows links accosiated with current animeID
-
-function getPage(){
-	var currentUrl
-	browser.tabs.query({currentWindow: true, active: true, url: "*://myanimelist.net/anime/*"})
-	  .then((tabs) => {
-		currentUrl = tabs[0].url
-	})
-	return currentUrl
-}
-
-//first extract MAL animeID from url. I am making an array with all the pathnames
-//eg https://myanimelist.net/anime/16498/Shingeki_no_Kyojin => anime/16498/Shingeki_no_Kyojin
-//anime/16498/Shingeki_no_Kyojin => {anime, 16498, Shingeki_no_Kyojin}. animeID is the number
-
-var animeID = new URL(getPage).pathname.split("/")[1];
-var database = browser.storage.local.get("JsonUrl");
-//find the link to the anime in the database
-var links = database[animeID]
-
-//look at all the links under the animeID, and create a button for each
-/*
-{links} should be an something like this:
-"link name": "link", "link name 2": "link 2", ......
-*/
-for (let i in links) {
-	var Hlink = document.createElement("Hlink")
-	Hlink.textContent = i
-	Hlink.href = links[i]
-	document.body.appendChild(Hlink)
-}
+browser.tabs.query({currentWindow: true, active: true})
+	.then((tabs) => {
+	let animeID = new URL(tabs[0].url).pathname.split("/")[2]
+	browser.storage.local.get("JsonUrl")
+		.then( (JsonUrl) => fetch(JsonUrl["JsonUrl"]))
+		.then( (response) => response.json())
+		.then( (database) => {
+			let links = database[animeID]
+			for (let i in links) {
+				var Hlink = document.createElement("Hlink")
+				Hlink.textContent = i
+				Hlink.href = links[i]
+				document.getElementById("PopupForm").appendChild(Hlink)
+			}
+		})
+})
