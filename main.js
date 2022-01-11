e102b8dd36a35fd46e886eb7ca32e8295b9b56a3
bananathepirate <bananathepirate@gmail.com>
2022-01-11 09:56:37 +0100

    fixed get episode number bug

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ let watched = document.getElementById("myinfo_watchedeps").value
 
 browser.runtime.onMessage.addListener((message) => {
 	console.log(message, watched)
-	return Promise.resolve(watched)
-	/*
 	if (message == "GetWatched") {
+		browser.runtime.sendMessage(watched)
 		return Promise.resolve(watched)
 	}
-	*/
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -14,19 +14,20 @@ function AddLink(linkArray, currentLink) {
 browser.tabs.query({currentWindow: true, active: true})
 .then((tabs) => {
 	browser.tabs.sendMessage(tabs[0].id, "GetWatched")
-	.then( (response) => {
-		console.log("response:: ", response)
-		watched = parseInt(response)
+	browser.runtime.onMessage.addListener( (message) => {
+		console.log("response:: ", message)
+		watched = parseInt(message)
 			// find MAL animeID from URL
 		let animeID = new URL(tabs[0].url).pathname.split("/")[2]
 		//get the link database
 		database = {
 			"40834": {
+				"link name": "link",
+
 				"2": {
 					"link name episode 2": "link episode 2",
 					"link name episode 2 a": "link episode 2 a"
-				},
-				"link name": "link"
+				}
 			}
 		}
 		let links = database[animeID]
@@ -44,7 +45,5 @@ browser.tabs.query({currentWindow: true, active: true})
 			}
 		}
 	})
-	.catch( (err) => console.log("ERROR", err))
-	console.log("something")
 })
 //id="myinfo_watchedeps" get value
