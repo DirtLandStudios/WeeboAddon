feec1dfdba1cabb42a3cb86fd2bb2220c42403e7
bananathepirate <bananathepirate@gmail.com>
2022-01-11 21:58:44 +0100

    added back actual JSON, now nothing works again

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,3 @@
-
-//let watched
-
-
 function AddLink(linkArray, currentLink) {
 	var Hlink = document.createElement("Hlink")
 	Hlink.textContent = currentLink
@@ -9,33 +5,30 @@ function AddLink(linkArray, currentLink) {
 	document.getElementById("PopupForm").appendChild(Hlink)
 }
 
-//summary: async is a mess and I hate it and myself and I want to die
-	//first, get current tab URL
 browser.tabs.query({currentWindow: true, active: true})
 .then((tabs) => {
+	var animeID = new URL(tabs[0].url).pathname.split("/")[2]
 	browser.tabs.sendMessage(tabs[0].id, "GetWatched")
 	browser.runtime.onMessage.addListener( (message) => {
-		watched = parseInt(message)
-			// find MAL animeID from URL
-		let animeID = new URL(tabs[0].url).pathname.split("/")[2]
-		//get the link database
-
-		let links = database[animeID]
-		
-		for (i in links) {
-			const Int_i = parseInt(i)
-			browser.tabs.sendMessage(tabs[0].id, Int_i)
-
-			if (Int_i == watched + 1) {
-				episodelinks = links[i]
-				for (episode_i in episodelinks) {
-					AddLink(episodelinks, episode_i)
+		var watched = parseInt(message)
+		browser.storage.local.get("JsonUrl")
+		.then( (JsonUrl) => fetch(JsonUrl["JsonUrl"]).json())
+		.then( (database) => {
+			browser.tabs.sendMessage(tabs[0].id, database)
+			let links = database[animeID]
+			for (i in links) {
+				let Int_i = parseInt(i)
+				browser.tabs.sendMessage(tabs[0].id, Int_i)
+				if (Number.isNaN(Int_i)) {
+					AddLink(links, i)
+				}
+				else if (Int_i == watched + 1) {
+					let episodelinks = links[i]
+					for (episode_i in episodelinks) {
+						AddLink(episodelinks, episode_i)
+					}
 				}
 			}
-			if (Number.isNaN(Int_i)) {
-				AddLink(links, i)
-			}
-		}
+		})
 	})
 })
-//id="myinfo_watchedeps" get value
