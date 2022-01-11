2cec4de202443c37cf9454ecf67aa0b98f2d7508
bananathepirate <bananathepirate@gmail.com>
2022-01-11 10:35:43 +0100

    Links Bug Fixed

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ browser.runtime.onMessage.addListener((message) => {
 	console.log(message, watched)
 	if (message == "GetWatched") {
 		browser.runtime.sendMessage(watched)
-		return Promise.resolve(watched)
 	}
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -15,33 +15,25 @@ browser.tabs.query({currentWindow: true, active: true})
 .then((tabs) => {
 	browser.tabs.sendMessage(tabs[0].id, "GetWatched")
 	browser.runtime.onMessage.addListener( (message) => {
-		console.log("response:: ", message)
 		watched = parseInt(message)
 			// find MAL animeID from URL
 		let animeID = new URL(tabs[0].url).pathname.split("/")[2]
 		//get the link database
-		database = {
-			"40834": {
-				"link name": "link",
 
-				"2": {
-					"link name episode 2": "link episode 2",
-					"link name episode 2 a": "link episode 2 a"
-				}
-			}
-		}
 		let links = database[animeID]
 		
-		for (let i in links) {
-			let Int_i = parseInt(i)
-			if (Int_i == NaN) {
-				AddLink(links, i)
-			}
-			else if (Int_i == watched + 1) {
-				let episodelinks = links[i]
-				for (let episode_i in episodelinks) {
+		for (i in links) {
+			const Int_i = parseInt(i)
+			browser.tabs.sendMessage(tabs[0].id, Int_i)
+
+			if (Int_i == watched + 1) {
+				episodelinks = links[i]
+				for (episode_i in episodelinks) {
 					AddLink(episodelinks, episode_i)
 				}
+			}
+			if (Number.isNaN(Int_i)) {
+				AddLink(links, i)
 			}
 		}
 	})
