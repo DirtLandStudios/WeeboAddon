fd28cc5700336edb6d6fdba6694efc025e462b49
bananathepirate <bananathepirate@gmail.com>
2022-02-22 18:45:59 +0100

    nothing works and I hate JS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 let watched = document.getElementById("myinfo_watchedeps").value
 
-browser.runtime.onMessage.addListener((message) => {
+/* browser.runtime.onMessage.addListener((message) => {
 	console.log(message, watched)
 	if (message == "GetWatched") {
 		browser.runtime.sendMessage(watched)
 		console.log(watched)
 	}
 })
+ */
+browser.runtime.sendMessage(watched)
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,35 +1,37 @@
 function AddLink(linkArray, currentLink) {
-	var Hlink = document.createElement("Hlink")
+	let Hlink = document.createElement("Hlink")
 	Hlink.textContent = currentLink
 	Hlink.href = linkArray[currentLink]
 	document.getElementById("PopupForm").appendChild(Hlink)
 }
 
 browser.tabs.query({currentWindow: true, active: true})
-.then((tabs) => {
-	var animeID = new URL(tabs[0].url).pathname.split("/")[2]
-	browser.tabs.sendMessage(tabs[0].id, "GetWatched")
-	browser.runtime.onMessage.addListener( (message) => {
-		watched = parseInt(message)
-		console.log(watched)
-		JsonUrl = await browser.storage.local.get("JsonUrl")
-		console.log(JsonUrl)
-		database = await fetch(JsonUrl["JsonUrl"])
-		console.log(database)
-		links = database[animeID]
-		for (i in links) {
-			//check for batch anime
-			let Int_i = parseInt(i)
-			if (Number.isNaN(Int_i)) {
-				AddLink(links, i)
-			}
-			//check for episodic anime
-			else if (Int_i == watched + 1) {
-				let episodelinks = links[i]
-				for (episode_i in episodelinks) {
-					AddLink(episodelinks, episode_i)
-				}
+	.then( (tabs) => {
+		let CurrentTab = tabs[0]
+		var animeID = new URL(CurrentTab.url).pathname.split("/")[2]
+		//browser.tabs.sendMessage(CurrentTab.id, "GetWatched")
+})
+
+// browser.tabs.executeScript({file: "/src/main.js"})
+
+
+browser.runtime.onMessage.addListener( (message) => {
+	let watched = parseInt(message)
+	let Json = await browser.storage.local.get("Database")
+	let database = Json //JSON.parse(Json)
+	let links = database[animeID]
+	for (i in links) {
+		//check for batch anime
+		let Int_i = parseInt(i)
+		if (Number.isNaN(Int_i)) {
+			AddLink(links, i)
+		}
+		//check for episodic anime
+		else if (Int_i == watched + 1) {
+			let episodelinks = links[i]
+			for (episode_i in episodelinks) {
+				AddLink(episodelinks, episode_i)
 			}
 		}
-	})
+	}
 })
